b3defc23f811b3391ca3615b91e38781b3e0e73d
lstubdal <lotte.stubdal@gmail.com>
2022-03-24 20:11:48 +0100

    Create Logo component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header">
+        <Logo />
+        <InformationCard />
+    </header>
+</template>
+
+<script>
+    import Logo from '../components/Logo.vue';
+    import InformationCard from '../components/InformationCard.vue';
+
+    export default {
+        components: {
+            Logo,
+            InformationCard,
+        }
+
+    }
+</script>
+
+<style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: fixed;
+        width: 100vw;
+        padding: var(--padding-small);
+    }
+
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+    <RouterLink :to="{name: 'home' }">
+        <img src="/icons/logo.svg" alt="rema1000 logo">
+    </RouterLink>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+</style>
