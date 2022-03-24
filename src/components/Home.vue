094adfd327708f2e6b3eed5bc080d57588fe5c05
lstubdal <lotte.stubdal@gmail.com>
2022-03-24 09:59:15 +0100

    Add InformationCard comp.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>vue boilerplate </div>
+   <div class="dashboard">
+       <header class="header">
+           <RouterLink :to="{name: 'home' }">
+                <img src="/icons/logo.svg" alt="rema1000 logo">
+           </RouterLink>
+
+           <InformationCard />
+       </header>
+   </div>
 </template>
 
 <script>
+    import InformationCard from '../components/InformationCard.vue';
 
     export default {
-   
+        components: {
+            InformationCard
+        }
     }
 </script>
 
 <style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: fixed;
+        width: 100vw;
+        padding: var(--padding-small);
+    }
 </style>
---
+++ src/components/InformationCard.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="informationCard">
+        <img @click="displayInformation" src="/icons/information.svg" alt="information icon" class="informationCard__icon">
+        
+        <section v-if="clicked" class="informationCard__content">
+            <img src="/icons/close.svg" alt="close icon" class="informationCard__close" @click="displayInformation">
+            <h2 class="informationCard__title">WHAT DOES THIS WEBSITE DO?</h2>
+            <p class="informationCard__paragraph">Search for any place/location to get information about the sunrise, sunset and moonrise for the given place. </p>
+            <p class="informationCard__paragraph">Note that some days may lack sunrise/sunset, this indicates that either polar night or polar day is in effect, and some days may lack moonrise, and will show 'no moon' </p>
+            <p class="informationCard__paragraph">This website uses information from Meterologisk institution in Norway.</p>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            clicked: false
+        }
+    },
+
+    methods: {
+        displayInformation() {
+            this.clicked = !this.clicked;
+        },
+
+        async test() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    .informationCard__icon {
+        cursor: pointer;
+    }
+
+    .informationCard__title {
+        color: var(--light);
+        font-size: 2em;
+        padding-bottom: var(--padding-small);
+    }
+
+    .informationCard__content {
+        position: absolute;
+        top: -20;
+        left: 0;
+        width: 100vw;
+        height: 65vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--main-color);
+    }
+
+    .informationCard__close {
+        position: absolute;
+        top: 30;
+        right: 35;
+        cursor: pointer;
+    }
+
+    .informationCard__paragraph {
+        width: 60%;
+        font-size: 1.5em;
+        font-weight: 300;
+        text-decoration: none;
+        padding: 1%;
+        color: var(--light);
+    }
+
+</style>
