f39cc3c10d583bd2bb90e6ecb8a3cc1da4d93193
lstubdal <lotte.stubdal@gmail.com>
2022-03-24 11:11:45 +0100

    Add storeinformation section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-   <div class="dashboard">
-       <header class="header">
-           <RouterLink :to="{name: 'home' }">
+    <div class="dashboard">
+        <header class="header">
+            <RouterLink :to="{name: 'home' }">
                 <img src="/icons/logo.svg" alt="rema1000 logo">
-           </RouterLink>
+            </RouterLink>
 
-           <InformationCard />
-       </header>
+            <InformationCard />
+        </header>
 
         <div class="dashboard__headline">
             <h1 class="dashboard__title"> {{ title }}</h1>
@@ -14,7 +14,30 @@
             <p class="dashboard__lead">{{ lead }}</p>
         </div>
 
-   </div>
+        <main class="dashboard__main">
+            <section class="storeInformation">
+                <div class="storeInformation__detail"> 
+                    <img src="/icons/location.svg" alt="location icon">
+                    <span class="storeInformation__address">Adresse:</span> 
+                    <p class="storeInformation__text">{{ address }}</p>
+                </div>
+
+                <div class="storeInformation__detail">  
+                    <img src="/icons/clock.svg" alt="clock icon">
+                    <span class="storeInformation__openingHours">Åpnignstider:</span>  
+                    <p class="storeInformation__text">{{ openingHours }}</p>
+                </div>
+
+                <div class="storeInformation__detail"> 
+                    <img src="/icons/mail.svg" alt="mail icon">
+                    <span class="storeInformation__mail">Mail:</span> 
+                    <p class="storeInformation__text">{{ mail }}</p>
+                </div>
+            </section>
+
+            <section class="map" id="map">map</section> <!-- shows map --> 
+        </main>
+    </div>
 </template>
 
 <script>
@@ -24,7 +47,10 @@
         data() {
             return {
                 title: 'Rema1000 oversikt',
-                lead: 'Her kan du finne alle Rema1000 butikker i Norge, samt adresse, åpningstid og mail'
+                lead: 'Her kan du finne alle Rema1000 butikker i Norge, samt adresse, åpningstid og mail',
+                address: '',
+                openingHours: '',
+                mail: ''
             }
         },
 
@@ -36,6 +62,10 @@
 
 <style>
     .dashboard {
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         font-family: var(--main-font);
     }
 
@@ -53,13 +83,13 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        margin-top: var(--margin-xlarge);
     }
 
     .dashboard__title {
-        font-size: 2.5em;
+        font-size: 2.7em;
         color: var(--dark);
-        font-weight: 200;
-        padding-top: 5%;
+        font-weight: 150;
     }
 
     .dashboard__line {
@@ -71,8 +101,36 @@
 
     .dashboard__lead {
         text-align: center;
-        font-size: 1.5em;
-        font-weight: 300;
-        padding: 1% 28%;
+        font-size: 1.8em;
+        font-weight: 250;
+        padding: 1% 20%;
     }
+
+    .dashboard__main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: var(--margin-medium);
+    }
+
+    .storeInformation {
+        display: flex;
+    }
+
+    .storeInformation__detail {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: var(--padding-large);
+    }
+
+    .storeInformation__address, .storeInformation__openingHours, .storeInformation__mail {
+        font-size: 1.5em;
+        font-weight: 200;
+        padding: var(--padding-small);
+    }
+
+
 </style>
