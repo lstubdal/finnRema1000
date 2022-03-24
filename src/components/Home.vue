305e18f6a034fdcd414d7263af41a9234fa4543d
lstubdal <lotte.stubdal@gmail.com>
2022-03-24 18:40:42 +0100

    Add mapbox-gl and markers

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,27 +35,115 @@
                 </div>
             </section>
 
-            <section class="map" id="map">map</section> <!-- shows map --> 
+            <div class="map" id="map"></div> <!-- shows map --> 
         </main>
     </div>
 </template>
 
 <script>
     import InformationCard from '../components/InformationCard.vue';
+    import mapboxgl from 'mapbox-gl';
 
     export default {
         data() {
             return {
                 title: 'Rema1000 oversikt',
                 lead: 'Her kan du finne alle Rema1000 butikker i Norge, samt adresse, åpningstid og mail',
+                mapbox_token: import.meta.env.VITE_MAPBOX_TOKEN,  // get token hidden token from .env.dev.
                 address: '',
                 openingHours: '',
-                mail: ''
+                mail: '',
+                storeData: [],
+                storeFeatures: []
             }
+        },
+
+        created() {
+            this.pageSetup();
         },
 
         components: {
             InformationCard
+        },
+
+        methods: {
+            async pageSetup() {
+                const urlWithProxy = `https://api.allorigins.win/get?url=${encodeURIComponent('https://rema.no/api/v2/stores')}`;   // using proxy to get pass CORS without authentication source: https://github.com/gnuns/allorigins  
+                const response = await fetch(urlWithProxy);
+                const { contents} = await response.json();  // recieves all API data as a string
+
+                const contentsAsObject = JSON.parse(contents)   // parse string to object to access array with store data
+                this.storeData = contentsAsObject.results  // save store data from api in array
+
+                this.createJsonObjects();
+                this.displayMap();
+            },
+
+            displayMap() {
+                mapboxgl.accessToken = this.mapbox_token;   // access token
+                const map = new mapboxgl.Map({
+                    container: 'map', // container ID
+                    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                    center:  [11.9413731, 65.1928539], // starting position [lng, lat] 
+                    zoom: 5 //starting zoom
+                });
+
+                this.createMarker(map);
+                // source: https://docs.mapbox.com/mapbox-gl-js/example/simple-map/ 
+            },
+
+            createJsonObjects() {
+                /* Fill empty array with mapped store json-objects to use in createMarker */
+                this.storeFeatures = this.storeData.map(store => {
+                    return {
+                        'type': 'Feature',
+                        'properties': {
+                            'name': store.name,
+                            'address': store.visitAddress,
+                            'openingHours': store.openingHours,
+                            'nettside': store.detailUrl,
+                            'phone': store.phone,
+                            'postInStore': store.postInStore,
+                            'iconSize': [25, 31]
+                            },
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [store.longitude, store.latitude]
+                            }
+                    }
+                })
+            },
+
+            createMarker(map) {
+                // store-json (aka geojson) iterate in feature array with json ojects created */
+                const storeJson = {
+                    'type': 'FeaturesCollection',
+                    'features': this.storeFeatures
+                }
+
+                // create custom marker per store
+                storeJson.features.forEach(store => {
+                    const marker = document.createElement('div');
+                    const width = store.properties.iconSize[0];
+                    const height = store.properties.iconSize[1];
+                    marker.className = 'marker';
+                    marker.style.backgroundImage = 'url(./icons/locationTag.svg)'
+                    marker.style.width = `${width}px`; 
+                    marker.style.height = `${height}px`;
+                    marker.style.backgroundSize = '100%';
+
+                    // show store data based on location of marker 
+                    marker.addEventListener('click', () => {    
+                        window.alert('KLIKKET');
+                    });
+
+                    // Add markers to the map.
+                    new mapboxgl.Marker(marker)
+                        .setLngLat(store.geometry.coordinates)
+                        .addTo(map);
+                })
+                // soruce: https://docs.mapbox.com/mapbox-gl-js/example/custom-marker-icons/ 
+            }
         }
     }
 </script>
@@ -67,6 +155,7 @@
         flex-direction: column;
         align-items: center;
         font-family: var(--main-font);
+        
     }
 
     .header {
@@ -126,11 +215,27 @@
         padding: var(--padding-large);
     }
 
-    .storeInformation__address, .storeInformation__openingHours, .storeInformation__mail {
+    .storeInformation__address, 
+    .storeInformation__openingHours, 
+    .storeInformation__mail {
         font-size: 1.5em;
         font-weight: 200;
         padding: var(--padding-small);
     }
 
+    #map {
+        border-radius: var(--corner-radius);
+        height: 100vh;
+        width: 80vw;
+        margin-bottom: var(--margin-large);
 
+    }
+
+    .marker {
+        display: block;
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+        padding: 0;
+    }
 </style>
